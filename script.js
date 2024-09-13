119d7dd33e1e44923fec117fc47821c4e3f74843
Caio Gonçalves <caiogoncalves101@gmail.com>
2024-09-12 22:10:32 -0300

    Correção do ponto flutuante

--- script.js
+++ script.js
@@ -1,34 +1,45 @@
-console.log("Teste");
 operadorAtivo = false;
 resultado = document.getElementById("resultado").innerHTML;
+
 function insert(numero) {
   resultado = document.getElementById("resultado").innerHTML;
+
   if (resultado.length < 50) {
     console.log(resultado);
+
+    // Se o resultado for "0", apenas substituímos o valor se o número não for um operador
     if (resultado == "0") {
       if (
-        numero == "." ||
         numero == "+" ||
         numero == "-" ||
         numero == "/" ||
         numero == "*" ||
         numero == "."
       ) {
+        if (numero == "." && resultado.includes(".")) return; // Verifica se já tem ponto
         document.getElementById("resultado").innerHTML = resultado + numero;
         operadorAtivo = true;
       } else {
         document.getElementById("resultado").innerHTML = numero;
       }
     } else if (
-      numero == "." ||
       numero == "+" ||
       numero == "-" ||
       numero == "/" ||
       numero == "*" ||
       numero == "."
     ) {
-      if (operadorAtivo) {
-      } else {
+      if (
+        numero == "." &&
+        resultado
+          .split(/[\+\-\*\/]/)
+          .pop()
+          .includes(".")
+      ) {
+        return; // Impede a inserção de mais de um ponto por número
+      }
+
+      if (!operadorAtivo) {
         document.getElementById("resultado").innerHTML = resultado + numero;
         operadorAtivo = true;
       }
@@ -40,9 +51,11 @@ function insert(numero) {
     alert("Você atingiu o máximo de caracteres");
   }
 }
+
 function reset() {
   document.getElementById("resultado").innerHTML = "0";
 }
+
 function apagar() {
   resultado = document.getElementById("resultado").innerHTML;
   if (resultado.length <= 1) {
@@ -54,6 +67,7 @@ function apagar() {
     );
   }
 }
+
 function calcular() {
   resultado = document.getElementById("resultado").innerHTML;
   resultadoFinal = eval(resultado);
